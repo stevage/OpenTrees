bfe4ea383075fc1de8121b0e138a8820eed100ab
Steve Bennett <stevage@gmail.com>
2015-04-06 20:21:18 +1000

    Record non-uniqueness of species.

--- makespecies.sql
+++ makespecies.sql
@@ -5,3 +5,12 @@ FROM alltrees
 group BY genus,species
 
 CREATE INDEX species_count ON species (count );
+
+ALTER TABLE alltrees
+ADD species_count integer;
+
+UPDATE alltrees
+SET species_count=count
+FROM species
+WHERE species.species=alltrees.species AND species.genus=alltrees.genus
+AND alltrees.species <> '' AND alltrees.genus <> '';
