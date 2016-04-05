ded6bb9f8720c9bcd902e0341ef980591132feb9
Steve Bennett <stevage@gmail.com>
2016-04-05 22:34:30 +1000

    Extra data

---
+++ extradata/getextras.sh
@@ -0,0 +1,3 @@
+wget 'http://datadryad.org/bitstream/handle/10255/dryad.59005/GlobalLeafPhenologyDatabase.csv?sequence=1'
+wget 'http://datadryad.org/bitstream/handle/10255/dryad.59002/GlobalWoodinessDatabase.csv?sequence=1'
+wget 'http://datadryad.org/bitstream/handle/10255/dryad.59001/Spermatophyta_Genera.csv?sequence=2'
